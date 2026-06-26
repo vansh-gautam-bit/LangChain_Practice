a88f34045293e10b5b1b0ce9912345373d7da4f1
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-26 11:34:53 +0530

    added the router for task 5

---
+++ app/task5/router.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException
+
+from app.task5.chain import safe_json_chain
+from app.task5.schemas import SafeJsonRequest, SafeJsonResponse
+
+router = APIRouter()
+
+@router.post(
+    "/safe-json",
+    response_model=SafeJsonResponse,
+)
+def safe_json(request: SafeJsonRequest):
+    
+    try:
+        result = safe_json_chain.invoke(
+            {
+                "text": request.text
+            }
+        )
+
+        return SafeJsonResponse(
+            data=result
+        )
+    
+    except Exception as e:
+        raise HTTPException(
+            stauts_code=42,
+            detail=str(e),
+        )
