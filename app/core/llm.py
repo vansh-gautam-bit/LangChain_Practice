95dd7b3271a31d6678e4daaf4c7a4a87766e86a7
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-26 11:37:49 +0530

    added  gemni model for different response

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,7 +1,13 @@
 from langchain_openrouter import ChatOpenRouter
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 from app.core.config import settings
 llm = ChatOpenRouter(
     model="cohere/north-mini-code:free",
     api_key=settings.OPENROUTER_API_KEY
+)
+
+llm2 = ChatGoogleGenerativeAI(
+    model ="gemini-2.5-flash",
+    api_key=settings.GEMINI_API_KEY
 )
