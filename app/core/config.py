cc1e9da4b26e33f68110ff4218412363083639cc
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-26 11:37:21 +0530

    added  gemni api for testing different model response

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     OPENROUTER_API_KEY: str
+    GEMINI_API_KEY: str
 
     model_config=SettingsConfigDict(
         env_file=".env",
