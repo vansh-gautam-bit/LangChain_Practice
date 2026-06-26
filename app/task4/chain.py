31e65973736680b6a768cb040930ce296f48f3d5
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-26 11:33:56 +0530

    added the chain for task 4

---
+++ app/task4/chain.py
@@ -0,0 +1,34 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel
+
+from app.core.llm import llm2
+
+from app.task4.prompts import (
+    summary_prompt,
+    tweet_prompt,
+    linkedin_prompt,
+)
+
+summary_chain = (
+    summary_prompt
+    | llm2
+    | StrOutputParser() 
+)
+
+tweet_chain = (
+    tweet_prompt
+    |llm2
+    | StrOutputParser()
+)
+
+linked_chain = (
+    linkedin_prompt
+    | llm2
+    | StrOutputParser()
+)
+
+content_chain = RunnableParallel(
+    summary=summary_chain,
+    tweet=tweet_chain,
+    linkedin_post=linked_chain,
+)
